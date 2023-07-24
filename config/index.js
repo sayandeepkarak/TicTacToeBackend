96ad0333c3860bc6975a14c2417e7929140f146d
Sayandeep Karak <sayandeep18.webdev@gmail.com>
2023-07-24 12:07:43 +0530

    update frontend access

--- config/index.js
+++ config/index.js
@@ -2,4 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const { PORT, DBURL, SECRET_KEY, SECRET_ACCESS_KEY } = process.env;
+export const { PORT, DBURL, SECRET_KEY, SECRET_ACCESS_KEY, FRONTEND_URL } =
+  process.env;
