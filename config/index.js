40e060dd9a1be401750d0353ddb1661c8ca62707
Sayandeep Karak <sayandeep18.webdev@gmail.com>
2023-07-24 16:27:18 +0530

    serve frontend production build

--- config/index.js
+++ config/index.js
@@ -2,5 +2,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const { PORT, DBURL, SECRET_KEY, SECRET_ACCESS_KEY, FRONTEND_URL } =
-  process.env;
+export const { PORT, DBURL, SECRET_KEY, SECRET_ACCESS_KEY } = process.env;
--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 import http from "http";
 import express from "express";
-import { DBURL, FRONTEND_URL,PORT } from "./config";
+import { DBURL, FRONTEND_URL, PORT } from "./config";
 import createSocketServer from "./events";
 import connectToDb from "./database";
-import cors from "cors";
+// import cors from "cors";
 import router from "./routes";
 import ErrorHandle from "./middlewares/errorHandler";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 const port = PORT || 6000;
 
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-    credentials: true,
-  })
-);
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: ["*"],
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use("/api/", router);
 app.use(ErrorHandle);
+
+app.use(express.static(path.join("./public")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 connectToDb(DBURL);
 
